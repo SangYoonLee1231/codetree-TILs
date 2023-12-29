ac70c38908eb1feee1489a58712b2a730532e521
SangYoonLee <prestro1231@hufs.ac.kr>
2023-12-29 09:46:06 +0900

    Title: 그래프 탐색, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231229/그래프 탐색/graph-traversal.py
+++ 231229/그래프 탐색/graph-traversal.py
@@ -1,28 +1,63 @@
+# Sol 1) 인접 리스트로 해결
+# n, m = tuple(map(int, input().split()))
+# graph = [[] for _ in range (n + 1)]
+# visited = [False for _ in range (n + 1)]
+# answer = -1
+
+# for _ in range(m):
+#     start, end = tuple(map(int, input().split()))
+#     graph[start].append(end)
+#     graph[end].append(start)
+
+# # print(graph)
+
+
+# # DFS 방식으로 그래프를 탐색하는 함수
+# def search(curr_v):
+#     for elem in graph[curr_v]:
+#         if not visited[elem]:
+#             visited[elem] = True
+#             search(elem)
+            
+
+# visited[1] = True
+# search(1)
+
+# for elem in visited:
+#     if elem:  answer += 1
+
+# print(answer)
+
+
+# Sol 2) 인접 행렬로 해결
 n, m = tuple(map(int, input().split()))
-lst = [[] for _ in range (n + 1)]
-visited = [False for _ in range (n + 1)]
+
+graph = [
+    [0] * (n + 1)
+    for _ in range(n + 1)
+]
+
+visited = [False for _ in range(n + 1)]
+
 answer = -1
 
 for _ in range(m):
     start, end = tuple(map(int, input().split()))
-    lst[start].append(end)
-    lst[end].append(start)
-
-# print(lst)
+    graph[start][end] = 1
+    graph[end][start] = 1
 
 
-# DFS 방식으로 그래프를 탐색하는 함수
 def search(curr_v):
-    for elem in lst[curr_v]:
-        if not visited[elem]:
-            visited[elem] = True
-            search(elem)
-            
+    for idx, elem in enumerate(graph[curr_v]):
+        if not visited[idx] and elem:
+            visited[idx] = True
+            search(curr_v)
+
 
 visited[1] = True
 search(1)
 
 for elem in visited:
-    if elem:  answer += 1
+    if elem:   answer += 1
 
 print(answer)
