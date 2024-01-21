8a9e7f8958f8161386d9ad579534a50115b5787d
SangYoonLee <prestro1231@hufs.ac.kr>
2024-01-21 22:33:52 +0900

    Title: 그 요일은, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240121/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,25 @@
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+target_day = input()
+
+week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+num_of_days = [0, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+start_day, end_day = 0, 0
+
+for month in range(1, m1):
+    start_day += num_of_days[month]
+start_day += d1
+
+for month in range(1, m2):
+    end_day += num_of_days[month]
+end_day += d2
+
+# Part 1
+gap = end_day - start_day
+answer = gap // 7
+
+# Part 2
+if gap % 7 > week.index(target_day):
+    answer += 1
+
+print(answer)
