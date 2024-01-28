54187057285c2108a368243b9c8fa647a1000703
SangYoonLee <prestro1231@hufs.ac.kr>
2024-01-29 01:06:47 +0900

    Title: 행복한 수열의 개수, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240129/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,56 @@
+n, m = tuple(map(int, input().split()))
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+answer = 0
+
+
+def search_row(r):
+    global answer
+
+    max_count = 0
+    count = 1
+
+    for c in range(n - 1):
+        if grid[r][c] == grid[r][c + 1]:
+            count += 1
+        else:
+            max_count = max(max_count, count)
+            count = 1
+    
+    max_count = max(max_count, count)
+    
+    if max_count >= m:
+        answer += 1
+
+
+def search_col(c):
+    global answer
+
+    max_count = 0
+    count = 1
+
+    for r in range(n - 1):
+        if grid[r][c] == grid[r + 1][c]:
+            count += 1
+        else:
+            max_count = max(max_count, count)
+            count = 1
+
+    max_count = max(max_count, count)
+    
+    if max_count >= m:
+        answer += 1
+
+
+
+for i in range(n):
+    search_row(i)
+
+for i in range(n):
+    search_col(i)
+
+print(answer)
