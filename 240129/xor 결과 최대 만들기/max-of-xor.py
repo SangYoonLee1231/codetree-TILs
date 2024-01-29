78f011ed04ba58bb0a67cea3110582031e8b7e06
SangYoonLee <prestro1231@hufs.ac.kr>
2024-01-29 18:18:44 +0900

    Title: xor 결과 최대 만들기, Time: 407ms, Memory: 73MB, Status: Passed - CodeTree

--- 240129/xor 결과 최대 만들기/max-of-xor.py
+++ 240129/xor 결과 최대 만들기/max-of-xor.py
@@ -1,33 +1,29 @@
 n, m = tuple(map(int, input().split()))
 lst = list(map(int, input().split()))
 chosen = []
-visited = [False] * n
 answer = 0
 
 
 def calc_xor():
     result = chosen[0]
-    for i in range(1, len(chosen)):
+    for i in range(1, m):
         result = result ^ chosen[i]
-    
+
     return result
 
 
-def backtrack(curr_num):
+def backtrack(curr_num, last_idx):
     global answer
 
     if curr_num == m:
         answer = max(answer, calc_xor())
         return
     
-    for idx, elem in enumerate(lst):
-        if not visited[idx]:
-            chosen.append(elem)
-            visited[idx] = True
-            backtrack(curr_num + 1)
-            visited[idx] = False
-            chosen.pop()
+    for idx in range(last_idx + 1, len(lst)):
+        chosen.append(lst[idx])
+        backtrack(curr_num + 1, idx)
+        chosen.pop()
 
 
-backtrack(0)
+backtrack(0, -1)
 print(answer)
