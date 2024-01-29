e676a5054291ef2dedc1eab3c8f0c48c819c7058
SangYoonLee <prestro1231@hufs.ac.kr>
2024-01-29 12:27:51 +0900

    Title: n개 중에 m개 뽑기, Time: 167ms, Memory: 25MB, Status: Passed - CodeTree

--- 240129/n개 중에 m개 뽑기/n-choose-m.py
+++ 240129/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,6 +1,6 @@
 n, m = tuple(map(int, input().split()))
 lst = []
-used = [False] * (n + 1)
+# used = [False] * (n + 1)
 
 
 def print_lst():
@@ -9,22 +9,22 @@ def print_lst():
     print()
 
 
-def backtrack(curr_n):
+def backtrack(curr_n, last_num):
     if curr_n >= m:
         print_lst()
         return
 
-    for i in range(1, n + 1):
-        if curr_n >= 1 and lst[-1] >= i:
-            continue
+    for i in range(last_num + 1, n + 1):
+        # if curr_n >= 1 and lst[-1] >= i:
+        #     continue
 
-        if not used[i]:
+        # if not used[i]:
             lst.append(i)
-            used[i] = True
-            backtrack(curr_n + 1)
-            used[i] = False
+            # used[i] = True
+            backtrack(curr_n + 1, i)
+            # used[i] = False
             lst.pop()
 
 
 
-backtrack(0)
+backtrack(0, 0)
