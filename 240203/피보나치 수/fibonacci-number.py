8939dcbc1ba7a37f4661b3aa847a2547b0b249ac
SangYoonLee <prestro1231@hufs.ac.kr>
2024-02-03 00:09:55 +0900

    Title: 피보나치 수, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/피보나치 수/fibonacci-number.py
+++ 240203/피보나치 수/fibonacci-number.py
@@ -2,9 +2,9 @@ n = int(input())
 dp = [-1] * (n + 1)
 
 for i in range(1, n + 1):
-    if n <= 2:
-        dp[n] = 1
+    if i <= 2:
+        dp[i] = 1
     else:
-        dp[n] = dp[n - 1] + dp[n - 2]
+        dp[i] = dp[i - 1] + dp[i - 2]
 
 print(dp[n])
