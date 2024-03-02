0c3b7035e0345d102470b7beb7a5a780ca78eeaa
SangYoonLee <prestro1231@hufs.ac.kr>
2024-03-02 20:05:20 +0900

    Title: 구구단 만들기 2, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240302/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,12 @@
+a, b = tuple(map(int, input().split()))
+
+for i in range(1, 5):
+    for j in range(b, a - 1, -1):
+        print(f"{j} * {i * 2} = {j * i * 2}", end="")
+
+        if j == a:
+            break
+        
+        print(" / ", end="")
+    
+    print()
