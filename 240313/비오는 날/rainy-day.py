d907dd66054fe9d487c83eb7b2e3924719bcf21e
SangYoonLee <prestro1231@hufs.ac.kr>
2024-03-13 04:34:45 +0900

    Title: 비오는 날, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240313/비오는 날/rainy-day.py
+++ 240313/비오는 날/rainy-day.py
@@ -18,8 +18,8 @@ rain_lst_len = len(rain_lst)
 # (1달이 모두 31일이라 가정 => 1년은 372일)
 for i in range(rain_lst_len):
     year, month, day = rain_lst[i][0].split('-')
-    time = year * 372 + month * 31 + day
-    rain_lst[i].append(day)
+    time = int(year) * 372 + int(month) * 31 + int(day)
+    rain_lst[i].append(time)
 
 rain_lst.sort(key=lambda x:x[3])
 rain_lst[0].pop()
