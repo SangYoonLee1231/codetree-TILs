ac917e411eba6dc2f819dc0185d6a504fe892ad3
SangYoonLee <prestro1231@hufs.ac.kr>
2024-03-15 17:35:59 +0900

    Title: 사각형 채우기, Time: 144ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240315/사각형 채우기/rectangle-fill.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+dp = [-1] * (n + 1)
+dp[n] = 1
+
+
+def dfs(curr_n):
+    dp[curr_n] = 0
+
+    for i in range(1, 3):
+        next_n = curr_n + i
+
+        if next_n > n:
+            continue
+
+        if dp[next_n] == -1:
+            dfs(next_n)
+        
+        dp[curr_n] += dp[next_n]
+
+
+dfs(0)
+print(dp[0])
