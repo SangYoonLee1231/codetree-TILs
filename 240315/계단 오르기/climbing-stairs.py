9f586aa6f6b0419786edd9e3e944a7247dab344f
SangYoonLee <prestro1231@hufs.ac.kr>
2024-03-15 17:12:09 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240315/계단 오르기/climbing-stairs.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+dp = [[-1] for _ in range(n + 1)]
+dp[n] = 0
+
+
+def dfs(curr_n):
+    dp[curr_n] = 0
+
+    for i in range(2, 4):
+        next_n = curr_n + i
+
+        if next_n > n:
+            continue
+
+        if dp[next_n] == -1:
+            dfs(next_n)
+
+        dp[curr_n] = dp[next_n] + 1
+
+
+dfs(0)
+print(dp[0])
