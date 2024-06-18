9b748d8c28075704b66fe113258f7757587b52ce
SangYoonLee (SYL) <prestro1231@hufs.ac.kr>
2024-06-18 16:15:37 +0900

    Title: 가장 많이 겹치는 구간, Time: 282ms, Memory: 30MB, Status: Passed - CodeTree

--- 240618/가장 많이 겹치는 구간/section-with-maximum-overlap.py
+++ 240618/가장 많이 겹치는 구간/section-with-maximum-overlap.py
@@ -4,14 +4,14 @@ sections = [
     for _ in range(n)
 ]
 
-checked = [0] * 201
-sum_val = [0] * 201
+checked = [0] * 200001
+sum_val = [0] * 200001
 
 for start, end in sections:
     checked[start] += 1
     checked[end] -= 1
 
-for i in range(1, 201):
+for i in range(1, 200001):
     sum_val[i] += checked[i] + sum_val[i - 1]
 
 print(max(sum_val))
