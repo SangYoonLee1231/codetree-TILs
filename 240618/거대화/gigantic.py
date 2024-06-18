4d2bec015d4c8997541cecfd5c861bd60bfc14e5
SangYoonLee (SYL) <prestro1231@hufs.ac.kr>
2024-06-18 17:34:52 +0900

    Title: 거대화, Time: 5102ms, Memory: 115MB, Status: Time Limit Exceed - CodeTree

---
+++ 240618/거대화/gigantic.py
@@ -0,0 +1,39 @@
+import sys
+sys.setrecursionlimit(100000)
+sys.set_int_max_str_digits(0)
+
+
+n, k = tuple(map(int, input().split()))
+
+num_lst = list(input())
+length = len(num_lst)
+answer = 0
+
+
+def check_biggest_num():
+    global answer
+    
+    temp_lst = []
+
+    for elem in num_lst:
+        if not elem:
+            continue
+        temp_lst.append(elem)
+
+    temp_num = int(''.join(map(str, temp_lst)))
+    answer = max(answer, temp_num)
+
+
+def backtrack(curr_idx, erased_count):
+    if erased_count == k:
+        check_biggest_num()
+
+    for i in range(curr_idx + 1, length):
+        temp = num_lst[i]
+        num_lst[i] = 0
+        backtrack(i, erased_count + 1)
+        num_lst[i] = temp
+
+
+backtrack(-1, 0)
+print(answer)
