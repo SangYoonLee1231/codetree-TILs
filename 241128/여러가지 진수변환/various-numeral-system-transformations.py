347c5881e939c95bc33002e8d33de656d8a8398b
SangYoonLee | SYL <prestro1231@hufs.ac.kr>
2024-11-28 10:55:56 +0900

    Title: 여러가지 진수변환, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241128/여러가지 진수변환/various-numeral-system-transformations.py
+++ 241128/여러가지 진수변환/various-numeral-system-transformations.py
@@ -4,11 +4,18 @@ n, b = tuple(map(int, input().split()))
 def func(n, b):
     rem_lst = []
 
-    while n >= b:
-        rem_lst.append(n % b)
-        n = n // b
+    # while n >= b:
+    #     rem_lst.append(n % b)
+    #     n = n // b
 
-    rem_lst.append(n)
+    # rem_lst.append(n)
+
+    while True:
+        if n == 0:
+            break
+        
+        rem_lst.append(n % b)
+        n //= b
 
     for elem in rem_lst[::-1]:
         print(elem, end='')
