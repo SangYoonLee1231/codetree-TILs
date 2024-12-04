356233538af3d015b5ae619d0a538bd653953eb2
Sang Yoon Lee <prestro1231@hufs.ac.kr>
2024-12-04 19:00:25 +0900

    Title: 왔다 갔던 구역 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241204/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,45 @@
+n = int(input())
+
+inputs = [
+    list(input().split())
+    for _ in range(n)
+]
+
+trail = [0 for _ in range(2001)]
+
+dl, dr = [1, -1]
+mapper = {
+    "R": 1,
+    "L": -1
+}
+
+
+def move():
+    global trail
+
+    curr = 1000
+
+    for i in range(n):
+        x = int(inputs[i][0])
+        dir_x = inputs[i][1]
+
+        for _ in range(x):
+            curr += mapper[dir_x]
+            trail[curr] += 1
+        curr += mapper[dir_x]
+    
+
+def check_answer():
+    global trail
+    
+    count = 0
+
+    for elem in trail:
+        if elem >= 2:
+            count += 1
+    
+    return count
+
+
+move()
+print(check_answer())
