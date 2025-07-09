2b0f7c5825ad4ffe8c8d3b08d19aa29ca3841ffc
Sang Yoon Lee (이상윤) <sylagape1231@gmail.com>
2025-07-09 13:45:05 +0900

    Title: 마을 구분하기, Time: 190ms, Memory: 15MB, Status: Passed - Codetree

--- 250709/마을 구분하기/seperate-village.js
+++ 250709/마을 구분하기/seperate-village.js
@@ -6,12 +6,13 @@ const inputGrid = input.slice(1, n + 1).map(line => line.split(' ').map(Number))
 
 // Please Write your code here.
 const [dr, dc] = [[1, -1, 0, 0], [0, 0, 1, -1]];
-const visitedGrid = Array.from({length: n}, () => new Array(n).fill(0));
+const visitedGrid = Array.from({length: n}, () => new Array(n).fill(false));
 const peopleArr = [];
 
+// 핵심 함수 1
 function dfs(r, c) {
     let cnt = 1;
-    visitedGrid[r][c] = 1;
+    visitedGrid[r][c] = true;
 
     for (let i = 0; i < 4; i++) {
         const [nr, nc] = [r + dr[i], c + dc[i]];
@@ -24,10 +25,7 @@ function dfs(r, c) {
     return cnt;
 }
 
-function inRange(r, c) {
-    return r >= 0 && r < n && c >= 0 && c < n;
-}
-
+// 핵심 함수 2
 function searchGrid() {
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
@@ -38,22 +36,7 @@ function searchGrid() {
     }
 }
 
-function canGo(r, c) {
-    // 1. 격자를 벗어나면 패스
-    if (!inRange(r, c)) {
-        return false;
-    }
-    // 2. 벽이 있으면 패스
-    if (!inputGrid[r][c]) {
-        return false;
-    }
-    // 3. 방문한 경우 패스
-    if (visitedGrid[r][c]) {
-        return false;
-    }
-    return true;
-}
-
+// 핵심 함수 3
 function makeAnswer() {
     searchGrid();
     peopleArr.sort((a, b) => a - b);
@@ -62,6 +45,18 @@ function makeAnswer() {
     peopleArr.forEach((elem) => {
         console.log(elem);
     })
+}
+
+// 보조 함수들
+function inRange(r, c) {
+    return r >= 0 && r < n && c >= 0 && c < n;
+}
+
+function canGo(r, c) {
+    // 1. 격자를 벗어나면 패스
+    // 2. 벽이 있으면 패스
+    // 3. 방문한 경우 패스
+    return inRange(r, c) && inputGrid[r][c] && !visitedGrid[r][c];
 }
 
 makeAnswer();
