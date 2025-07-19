7d29446704cedfcba40bc2718b23d01d94010463
Sang Yoon Lee (이상윤) <sylagape1231@gmail.com>
2025-07-19 19:13:54 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1098ms, Memory: 32MB, Status: Time Limit Exceed - Codetree

---
+++ 250719/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,41 @@
+n, m = map(int, input().split())
+snake_grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+grid = [
+    [0] * m
+    for _ in range(n)
+]
+
+visited = [
+    [False] * m
+    for _ in range(n)
+]
+
+drs, dcs = [1, 0], [0, 1]
+
+
+def dfs(r, c, depth):
+    for dr, dc in zip(drs, dcs):
+        nr, nc = r + dr, c + dc
+
+        if can_go(nr, nc):
+            visited[nr][nc] = 1
+            grid[nr][nc] = depth
+
+            dfs(nr, nc, depth + 1)
+
+            visited[nr][nc] = 0
+
+
+def can_go(r, c):
+    return in_range(r, c) and not visited[r][c] and snake_grid[r][c]
+
+
+def in_range(r, c):
+    return r >= 0 and r < n and c >= 0 and c < m
+
+
+dfs(0, 0, 1)
+
+print(1 if grid[n - 1][m - 1] else 0)
