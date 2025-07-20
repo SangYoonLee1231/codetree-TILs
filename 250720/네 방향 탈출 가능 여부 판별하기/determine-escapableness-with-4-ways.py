350a2d1afa5157d6963f72a0e5d61f37c874d524
Sang Yoon Lee (이상윤) <sylagape1231@gmail.com>
2025-07-20 17:52:45 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 101ms, Memory: 20MB, Status: Passed - Codetree

---
+++ 250720/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+n, m = map(int, input().split())
+snake_grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+drs, dcs = [0, 0, 1, -1], [1, -1, 0, 0]
+
+grid = [
+    [0] * m
+    for _ in range(n)
+]
+
+q = deque()
+
+
+def bfs():
+    while q:
+        r, c = q.popleft()
+
+        for dr, dc in zip(drs, dcs):
+            nr, nc = r + dr, c + dc
+
+            if can_go(nr, nc):
+                grid[nr][nc] = 1
+                q.append((nr, nc))
+
+
+def can_go(r, c):
+    return in_range(r, c) and not grid[r][c] and snake_grid[r][c]
+
+
+def in_range(r, c):
+    return r >= 0 and r < n and c >= 0 and c < m
+
+
+
+grid[0][0] = 1
+q.append((0, 0))
+bfs()
+
+print(grid[n - 1][m - 1])
