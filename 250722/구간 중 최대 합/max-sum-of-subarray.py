9a67a83159e38ca322ddf683e5fa4045bd41fb6c
Sang Yoon Lee (이상윤) <sylagape1231@gmail.com>
2025-07-22 23:23:21 +0900

    Title: 구간 중 최대 합, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250722/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+for i in range(n - k):
+    max_sum = 0
+    temp_sum = 0
+
+    for j in range(k):
+        temp_sum += arr[i + j]
+
+    max_sum = max(max_sum, temp_sum)
+
+print(max_sum)
